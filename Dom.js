6fc2f2ea3e777634b2f805431c721c3d34011fa8
Rajesh-gahilod1435 <120488895+Rajesh-gahilod1435@users.noreply.github.com>
2023-06-08 13:42:45 +0530

    Add files via upload

---
+++ Dom.js
@@ -0,0 +1,18 @@
+// let id= document.getElementById('id').innerText="Namaste";
+
+// let H1=document.getElementsByTagName('h1');
+// H1.innerHTML="hh";
+// console.log(H1);
+// console.log(H1);
+
+// "Print output: 
+var x = 21;
+var girl = function () {
+    console.log(x);
+    var x = 20;
+};  
+girl ();
+ 
+
+let elem=document.querySelectorAll('h1').innerHTML="Hello World 01";
+
